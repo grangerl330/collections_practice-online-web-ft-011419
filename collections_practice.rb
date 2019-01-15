0e0bed1faefb98daa39f4da806efcf48f420d42d
Luke Granger <grangerl330@gmail.com>
2019-01-15 00:25:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,4 +8,12 @@ end
 
 def sort_array_char_count(array)
   array.sort!{|word_1,word_2| word_1.length <=> word_2.length}
+end
+
+def swap_elements(array)
+  new_array = array
+  new_array[1] = array[2]
+  new_array[2] = array[1]
+  new_array
 end 
+  
