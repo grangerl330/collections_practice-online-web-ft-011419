64bdc9e7cd0da3d1b8d908b1264a12c894b45229
Luke Granger <grangerl330@gmail.com>
2019-01-15 00:23:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,5 +7,5 @@ def sort_array_desc(array)
 end 
 
 def sort_array_char_count(array)
-  array.sort!{|word_1,word_2| word_1 <=> word_2}
+  array.sort!{|word_1,word_2| word_1.length <=> word_2.length}
 end 
